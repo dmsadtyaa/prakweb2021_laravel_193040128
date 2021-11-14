a0ab4e987cdc28ddc92edf533d70c7d5c967540e
Dimas Aditya <90441390+dmsadtyaa@users.noreply.github.com>
2021-11-15 06:28:25 +0700

    Menyelesaikan Video Laravel

---
+++ prakweb2021_laravel_193040128-main/coba-laravel/app/Models/Post_.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+
+
+class Post 
+{
+   private static $blog_posts = [
+        [
+            "title" =>"Judul Post Pertama",
+            "slug" =>"judul-post-pertama",
+            "author" => "Dimas Aditya",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad libero necessitatibus facilis officiis omnis est eius placeat ea pariatur neque? Beatae totam doloribus fugit ipsum facere iste omnis numquam quod!
+            "
+        ],
+        [
+            "title" =>"Judul Post Kedua",
+            "slug" =>"judul-post-kedua",
+            "author" => "Dimas Aditya",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit similique adipisci blanditiis hic provident sapiente accusamus tempora incidunt laborum quaerat soluta recusandae, voluptatum voluptate, asperiores voluptates maiores consectetur aut quidem, vel ab excepturi sequi consequatur ratione. Deleniti maiores possimus, quaerat, repellendus facilis animi omnis sit architecto modi magni dolorum eum quos officia perferendis! Neque alias odio repellendus, maiores omnis aut minus nostrum accusantium sunt perspiciatis sapiente nam totam accusamus necessitatibus eaque similique sint, quod eos quia? Quae perferendis animi molestiae obcaecati optio velit excepturi esse minima, recusandae expedita fugiat tenetur dolorum ea consequatur laudantium sed sunt maxime, omnis quod corrupti! A odio, eligendi voluptatum consequatur vel obcaecati! Neque tenetur minima similique sunt accusantium voluptate optio, sed temporibus debitis. Molestiae libero perferendis voluptates non temporibus? Sit deleniti optio asperiores, quibusdam soluta, maxime dolorum possimus modi minima vero, commodi nulla. Itaque neque cupiditate officiis minima quod odio perspiciatis dolores mollitia. Dolore suscipit nobis nisi fugit aperiam nihil sint sed dolorum est eaque sunt explicabo reprehenderit deserunt saepe unde, corporis reiciendis beatae aut similique repellat praesentium quibusdam. Doloremque fugiat quae fuga, sapiente vero accusantium, laboriosam officia eveniet voluptatem rem natus velit quia beatae? Vel dolorem culpa consectetur? Quia quae quo voluptatibus laborum voluptas!"
+        ]
+        
+        ];
+
+        public static function all() 
+        {
+            return collect(self::$blog_posts);
+        }
+
+        public static function find($slug) 
+        {
+            $posts = static::all();
+            return $posts->firstWhere('slug', $slug);
+        }
+}
---
+++ prakweb2021_laravel_193040128-main/coba-laravel/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        // User::create([
+        //     'name' =>'Dimas Aditya',
+        //     'email' => 'masdit966@gmail.com',
+        //     'password' => dimasaditya('12345')
+        // ]);
+
+        // User::create([
+        //     'name' =>' Dimas Aditya',
+        //     'email' => 'masdit966@@gmail.com',
+        //     'password' => dimasaditya('12345')
+        // ]);
+        
+        User::factory(3)->create();
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::Factory(20)->create();
+
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur modi debitis velit, quam nulla nam! Dolores natus illo deserunt dolor deleniti libero est provident, quas odit?',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.</p><p> Consectetur modi debitis velit, quam nulla nam! Dolores natus illo deserunt dolor deleniti libero est provident, quas odit? Repudiandae earum, doloremque, aliquam accusamus molestiae voluptates nisi illo aspernatur cumque doloribus sint eius sunt deserunt voluptatem harum a esse commodi hic reiciendis? Fuga sit ex perferendis atque pariatur ad, accusamus repudiandae eius, quasi quis tempore non expedita error fugit explicabo aspernatur delectus veniam doloribus ab vel nemo illum animi. Asperiores, cupiditate quaerat. </p><p>Labore blanditiis explicabo iure placeat illo soluta consequatur? Hic, dolore. Sed cumque quidem rem et officia consectetur optio totam quaerat adipisci est corporis ut tempora distinctio recusandae nostrum minus dolor voluptatibus, sint sequi dicta deleniti. Ad cumque non consectetur consequuntur nostrum, natus nisi delectus repellat minima laudantium fugit expedita, exercitationem quod quidem quaerat quae. Possimus doloremque nisi, voluptates magni voluptatem quam mollitia quaerat ex libero distinctio accusantium molestias repudiandae labore aspernatur?</p>',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'slug' => 'judul-kedua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur modi debitis velit, quam nulla nam! Dolores natus illo deserunt dolor deleniti libero est provident, quas odit?',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.</p><p> Consectetur modi debitis velit, quam nulla nam! Dolores natus illo deserunt dolor deleniti libero est provident, quas odit? Repudiandae earum, doloremque, aliquam accusamus molestiae voluptates nisi illo aspernatur cumque doloribus sint eius sunt deserunt voluptatem harum a esse commodi hic reiciendis? Fuga sit ex perferendis atque pariatur ad, accusamus repudiandae eius, quasi quis tempore non expedita error fugit explicabo aspernatur delectus veniam doloribus ab vel nemo illum animi. Asperiores, cupiditate quaerat. </p><p>Labore blanditiis explicabo iure placeat illo soluta consequatur? Hic, dolore. Sed cumque quidem rem et officia consectetur optio totam quaerat adipisci est corporis ut tempora distinctio recusandae nostrum minus dolor voluptatibus, sint sequi dicta deleniti. Ad cumque non consectetur consequuntur nostrum, natus nisi delectus repellat minima laudantium fugit expedita, exercitationem quod quidem quaerat quae. Possimus doloremque nisi, voluptates magni voluptatem quam mollitia quaerat ex libero distinctio accusantium molestias repudiandae labore aspernatur?</p>',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
+        // Post::create([
+        //     'title' => 'Judul Ketiga',
+        //     'slug' => 'judul-ketiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur modi debitis velit, quam nulla nam! Dolores natus illo deserunt dolor deleniti libero est provident, quas odit?',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.</p><p> Consectetur modi debitis velit, quam nulla nam! Dolores natus illo deserunt dolor deleniti libero est provident, quas odit? Repudiandae earum, doloremque, aliquam accusamus molestiae voluptates nisi illo aspernatur cumque doloribus sint eius sunt deserunt voluptatem harum a esse commodi hic reiciendis? Fuga sit ex perferendis atque pariatur ad, accusamus repudiandae eius, quasi quis tempore non expedita error fugit explicabo aspernatur delectus veniam doloribus ab vel nemo illum animi. Asperiores, cupiditate quaerat. </p><p>Labore blanditiis explicabo iure placeat illo soluta consequatur? Hic, dolore. Sed cumque quidem rem et officia consectetur optio totam quaerat adipisci est corporis ut tempora distinctio recusandae nostrum minus dolor voluptatibus, sint sequi dicta deleniti. Ad cumque non consectetur consequuntur nostrum, natus nisi delectus repellat minima laudantium fugit expedita, exercitationem quod quidem quaerat quae. Possimus doloremque nisi, voluptates magni voluptatem quam mollitia quaerat ex libero distinctio accusantium molestias repudiandae labore aspernatur?</p>',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
+        // Post::create([
+        //     'title' => 'Judul Keempat',
+        //     'slug' => 'judul-keempat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur modi debitis velit, quam nulla nam! Dolores natus illo deserunt dolor deleniti libero est provident, quas odit?',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.</p><p> Consectetur modi debitis velit, quam nulla nam! Dolores natus illo deserunt dolor deleniti libero est provident, quas odit? Repudiandae earum, doloremque, aliquam accusamus molestiae voluptates nisi illo aspernatur cumque doloribus sint eius sunt deserunt voluptatem harum a esse commodi hic reiciendis? Fuga sit ex perferendis atque pariatur ad, accusamus repudiandae eius, quasi quis tempore non expedita error fugit explicabo aspernatur delectus veniam doloribus ab vel nemo illum animi. Asperiores, cupiditate quaerat. </p><p>Labore blanditiis explicabo iure placeat illo soluta consequatur? Hic, dolore. Sed cumque quidem rem et officia consectetur optio totam quaerat adipisci est corporis ut tempora distinctio recusandae nostrum minus dolor voluptatibus, sint sequi dicta deleniti. Ad cumque non consectetur consequuntur nostrum, natus nisi delectus repellat minima laudantium fugit expedita, exercitationem quod quidem quaerat quae. Possimus doloremque nisi, voluptates magni voluptatem quam mollitia quaerat ex libero distinctio accusantium molestias repudiandae labore aspernatur?</p>',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
+
+
+    }
+}
